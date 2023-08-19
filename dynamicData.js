84c9543dba5e914c59039790bb210926e5668d77
Rafal <rafalszkatula1@gmail.com>
2023-08-19 17:58:32 +0200

    mission complete

--- dynamicData.js
+++ dynamicData.js
@@ -1,39 +1,60 @@
-const dataContainer = document.querySelector('.card-details__data-container');
-fetch('data.json')
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(item => {
-            const itemContainer = document.createElement('div');
-            itemContainer.classList.add('card-details__score');
-            
-            const categoryIcon = document.createElement('img');
-            categoryIcon.classList.add('card-details__category-icon');
-            categoryIcon.src = item.icon;
-            categoryIcon.alt = item.category + ' icon';
-            
-            const categorySpan = document.createElement('span');
-            categorySpan.classList.add('card-details__category');
-            categorySpan.textContent = item.category;
-            categorySpan.classList.add('card-details__category-three');
-            
-            const scoreP = document.createElement('p');
-            scoreP.classList.add('card-details__result');
-            scoreP.textContent = item.score;
-            
-            const maxSpan = document.createElement('span');
-            maxSpan.classList.add('card-details__max');
-            maxSpan.textContent = '/100';
-            // 80/100
-            dataContainer.appendChild(itemContainer);
+function numToWord(number) {
+	switch (number) {
+		case 1:
+			return "one";
+		case 2:
+			return "two";
+		case 3:
+			return "three";
+		case 4:
+			return "four";
+		// Dodaj pozostałe przypadki w zależności od potrzeb
+		default:
+			return "unknown";
+	}
+}
+const dataContainer = document.querySelector(".card-details__data-container");
+fetch("data.json")
+	.then((response) => response.json())
+	.then((data) => {
+		data.forEach((item, index) => {
+			const itemContainer = document.createElement("div");
+			itemContainer.classList.add("card-details__score");
 
-            itemContainer.appendChild(categoryIcon);
-            itemContainer.appendChild(categorySpan);
-            itemContainer.appendChild(scoreP);
-            scoreP.appendChild(maxSpan);
-        })
-    })
-{/* <span class="card-details__category card-details__category-one"><i class="card-details__category-icon"></i>category</span>
+			const categoryIcon = document.createElement("img");
+			categoryIcon.classList.add("card-details__category-icon");
+			categoryIcon.src = item.icon;
+			categoryIcon.alt = item.category + " icon";
+
+			const categorySpan = document.createElement("span");
+			categorySpan.classList.add("card-details__category");
+			categorySpan.textContent = item.category;
+			const classCounter = "card-details__category-" + numToWord(index + 1);
+			categorySpan.classList.add(classCounter);
+
+			const scoreP = document.createElement("p");
+			scoreP.classList.add("card-details__result");
+			scoreP.textContent = item.score;
+
+			const maxSpan = document.createElement("span");
+			maxSpan.classList.add("card-details__max");
+			maxSpan.textContent = "/100";
+			// 80/100
+			dataContainer.appendChild(itemContainer);
+
+			itemContainer.appendChild(categoryIcon);
+			itemContainer.appendChild(categorySpan);
+			itemContainer.appendChild(scoreP);
+			scoreP.appendChild(maxSpan);
+		});
+	});
+{
+	/* <span class="card-details__category card-details__category-one"><i class="card-details__category-icon"></i>category</span>
 					<span class="card-details__result">
 						score
 						<span class="card-details__max">/100</span>
-					</span> */}
+					</span> */
+}
+const allDataContainers = document.querySelectorAll(".card-details__score");
+
+// categorySpan.classList.add('card-details__category-three');
